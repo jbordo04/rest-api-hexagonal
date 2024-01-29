c1699f566e32774b4c33ee04dc98b86e20540ab0
thejbordo <joan.bordonaba@gmail.com>
2024-01-29 23:36:33 +0100

    fix 16

--- src/indexJest.ts
+++ src/indexJest.ts
@@ -1,5 +1,19 @@
+import "dotenv/config"
 import { Server } from "./index"
+export const PORT = Number(process.env.PORT) || 4000
 
-const app = new Server(5000)
-app.start()
-app.stop()
+const app = new Server(3500)
+app.start().then(() => {
+  console.log(`[APP] - Server is running on: http://localhost:${PORT}/api`)
+
+  // Espera 5 segundos (5000 milisegundos) y luego detén el servidor
+  setTimeout(async () => {
+    console.log("Tiempo transcurrido. Cerrando el servidor...")
+
+    // Detén el servidor antes de salir
+    await app.stop()
+
+    // Sal del proceso
+    process.exit()
+  }, 5000)
+})
